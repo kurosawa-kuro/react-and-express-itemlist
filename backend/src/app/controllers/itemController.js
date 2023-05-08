7dd3cac7fc9ebe82856fb9fadd196d526948eb4b
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-05-08 23:43:50 +0900

    add asyncHandler

--- backend/src/app/controllers/itemController.js
+++ backend/src/app/controllers/itemController.js
@@ -1,15 +1,16 @@
 // Path: backend/src/app/controllers/itemController.js
 
+import asyncHandler from "express-async-handler";
 import { db } from "../../database/prisma/prismaClient.js";
 
-export const getAllItems = async (req, res) => {
+export const getAllItems = asyncHandler(async (req, res) => {
     const items = await db.item.findMany();
     res.json(items);
-};
+});
 
-export const createNewItem = async (req, res) => {
+export const createNewItem = asyncHandler(async (req, res) => {
     const newItem = await db.item.create({ data: req.body });
     res.status(201).json(newItem);
-};
+});
 
 export default { getAllItems, createNewItem };
