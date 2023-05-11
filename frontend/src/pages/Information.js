13da2d084444cbcbc835daef50b2f1da8029b10c
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-05-11 12:59:11 +0900

    add register

--- frontend/src/pages/Information.js
+++ frontend/src/pages/Information.js
@@ -5,6 +5,8 @@ const Information = () => {
         <div>
             <h1>Information</h1>
             <p>This is the information page.</p>
+            <br></br>
+            <p>Public data</p>
         </div>
     );
 };
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,10 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../styles/App.css';
 
 const Registration = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (!name || !email || !password) {
+            setError('Please fill out all fields');
+        } else {
+            // Send form data to server
+            console.log('Form data:', { name, email, password });
+            // Clear form data
+            setName('');
+            setEmail('');
+            setPassword('');
+            setError('');
+        }
+    };
+
     return (
-        <div>
+        <div className="container">
             <h1>Registration</h1>
-            <p>This is the registration page.</p>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="name">Name:</label>
+                    <input
+                        type="text"
+                        id="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        placeholder="Enter your name"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Enter your email"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="Enter your password"
+                    />
+                </div>
+                {error && <div className="error">{error}</div>}
+                <button type="submit">Submit</button>
+            </form>
         </div>
     );
 };
